7748b88c09e79a7ae983b3a2edd37bff21c94969
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-11-23 20:40:42 +0100

    feat: add number/index to tiles

--- discovery/src/components/DiscoveryTiles.vue
+++ discovery/src/components/DiscoveryTiles.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="grid grid-cols-5 gap-4">
-    <div v-for="(_, i) in Array(10)" class="h-8 bg-red-100 rounded-md"></div>
+    <div v-for="(_, i) in Array(10)" class="h-8 bg-red-100 rounded-md">
+      {{ i + 1 }}
+    </div>
   </div>
 </template>
